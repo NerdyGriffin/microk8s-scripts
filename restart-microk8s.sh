52eebb11b488f9bcaa4934b5d66aa65d61645fb0
Christian Kunis <ckunis98@gmail.com>
2025-05-24 10:01:01 -0400

    Port restart script from local node

---
+++ restart-microk8s.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+nodeArray=( "${@}" )
+if [ $# -eq 0 ]; then
+    read -p "Enter the hostname of the node you want to upgrade: " nodeName
+    nodeArray+=( $nodeName )
+fi
+echo "The Microk8s service will be restarted on the following nodes:"
+for nodeName in "${nodeArray[@]}"; do
+    nodeFQDN=$(sudo ssh "root@$nodeName" hostname)
+    echo "$nodeName = $nodeFQDN"
+done
+read -p "Continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+for nodeName in "${nodeArray[@]}"; do
+    nodeFQDN=$(sudo ssh "root@$nodeName" hostname)
+    # sudo ssh "root@$nodeFQDN" sudo microk8s stop
+    # sudo ssh "root@$nodeFQDN" sudo microk8s start
+    # sudo ssh "root@$nodeFQDN" sudo microk8s status --wait
+    sudo ssh "root@$nodeFQDN" << EOF
+sudo microk8s stop && sudo microk8s start && sudo microk8s status --wait
+EOF
+done
