be414ce80c1a40599fa2391a3117086086d3edcc
Christian Kunis <github@nerdygriffin.net>
2025-11-04 13:22:41 -0500

    Automate the upgrade/install of microk8s addons

--- upgrade-microk8s.sh
+++ upgrade-microk8s.sh
@@ -38,3 +38,42 @@ for nodeName in "${nodeArray[@]}"; do
     sleep 10
     sudo microk8s kubectl get node
 done
+echo 'Would you like to reinstall the core addons (forced upgrade)?'
+read -p "WARNING: This WILL result is downtime for all services and ingress (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+addonList=(
+  'cert-manager'
+  'dashboard'
+  'dns'
+  # 'ha-cluster'
+  # 'helm'
+  # 'helm3'
+  'ingress'
+  'metallb'
+  'metrics-server'
+  'observability'
+  'rbac'
+)
+for addonName in ${addonList[*]}; do
+    echo '----------------'
+    echo && echo "Disabling $addonName... "
+    sudo microk8s disable "$addonName"
+    for addonName in ${addonList[*]}; do
+        echo && echo "Enabling $addonName... "
+        case "$addonName" in
+            "dns")
+                sudo microk8s enable dns:1.1.1.1
+                ;;
+            *"metallb"*)
+                sudo microk8s enable "$addonName" '10.64.140.0/24'
+                ;;
+            *)
+                sudo microk8s enable "$addonName"
+                ;;
+        esac
+    done
+done
+sudo microk8s disable hostpath-storage:destroy-storage
+sudo microk8s kubectl -n kubernetes-dashboard patch svc kubernetes-dashboard-kong-proxy --patch='{"spec":{"loadBalancerIP":"10.64.140.8","type": "LoadBalancer"}}'
+sudo microk8s kubectl -n kube-system patch configmap/coredns --patch-file="$(dirname "$0")/coredns-patch.yaml"
+sudo microk8s kubectl -n kube-system patch svc kube-dns --patch='{"spec":{"loadBalancerIP":"10.64.140.10","type": "LoadBalancer"}}'
+sudo microk8s kubectl apply -f "$(dirname "$0")/ingress-service.yaml"
