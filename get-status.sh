079bce7b2fce4daa9a3ab6b8b5677a1257340414
Christian Kunis <github@nerdygriffin.net>
2025-11-05 17:09:37 -0500

    chore(get-status.sh): remove debug echo and uncomment kubectl commands for node status retrieval

--- get-status.sh
+++ get-status.sh
@@ -9,7 +9,6 @@ detect_kubectl
 readarray -t nodeArray < <(${KUBECTL} get nodes -o name 2>/dev/null | sed 's|node/||')
 for nodeFQDN in "${nodeArray[@]}"; do
     sshDest="root@$nodeFQDN"
-    echo "$sshDest"
-    # ${KUBECTL} get node "$nodeFQDN"
-    # ssh "$sshDest" microk8s status | head -n 4
+    ${KUBECTL} get node "$nodeFQDN"
+    ssh "$sshDest" microk8s status | head -n 4
 done
