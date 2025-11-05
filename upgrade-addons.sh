26a242b0d3a0156e603119ff2db956124a584fdb
Christian Kunis <github@nerdygriffin.net>
2025-11-05 13:56:59 -0500

    Refactor upgrade-addons.sh to improve user prompts and streamline addon management logic

--- upgrade-addons.sh
+++ upgrade-addons.sh
@@ -26,8 +26,7 @@ ensure_jq() {
 ensure_jq >/dev/null 2>&1 || echo "Continuing without jq (will use conservative JSON string checks)."
 function pause(){
     if [ -t 0 ]; then
-        sleep 20
-#        read -p 'Press [Enter] key to continue...'
+        read -p 'Press [Enter] key to continue...'
     else
         sleep 10
     fi
@@ -42,15 +41,10 @@ read -p "Continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][e
 for nodeFQDN in "${nodeArray[@]}"; do
     sshDest="root@$nodeFQDN"
     ${KUBECTL} get node "$nodeFQDN"
-    sudo microk8s disable hostpath-storage:destroy-storage
     sudo ssh "$sshDest" sudo snap alias microk8s.kubectl kubectl
     sudo ssh "$sshDest" sudo microk8s addons repo update core
     sudo ssh "$sshDest" 'sudo sed -i "s|^\(--resolv-conf=\).*$|\1/run/systemd/resolve/resolv.conf|" /var/snap/microk8s/current/args/kubelet'
-    ${KUBECTL} get node "$nodeFQDN"
 done
-echo 'Would you like to reinstall the core addons (forced upgrade)?'
-echo 'WARNING: This WILL result is downtime for all services and ingress.'
-read -p "Continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 addonList=(
   'cert-manager'
   'dashboard'
@@ -65,12 +59,16 @@ addonList=(
   'rbac'
 )
 ${KUBECTL} apply -f /var/snap/microk8s/common/addons/core/addons/metallb/crd.yaml
-for addonName in "${addonList[@]}"; do
-    echo '#--------------------------------'
-    echo "Disabling $addonName... "
-    sudo microk8s disable "$addonName"
-done
-${KUBECTL} delete -f /var/snap/microk8s/common/addons/core/addons/metallb/crd.yaml
+echo 'Would you like to reinstall the core addons (forced upgrade)?'
+echo 'WARNING: This WILL result is downtime for all services and ingress.'
+read -p "Continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] && {
+    for addonName in "${addonList[@]}"; do
+        echo '#--------------------------------'
+        echo "Disabling $addonName... "
+        sudo microk8s disable "$addonName"
+    done
+    ${KUBECTL} delete -f /var/snap/microk8s/common/addons/core/addons/metallb/crd.yaml
+}
 sudo microk8s disable hostpath-storage:destroy-storage
 for addonName in "${addonList[@]}"; do
     echo '#--------------------------------'
